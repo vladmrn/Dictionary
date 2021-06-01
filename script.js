3425c3ed81fca6b6f33455e707ce52941de2f954
vlad1427 <vlad1427@yahoo.com>
2021-06-01 13:35:08 +0100

    implementing find method

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ function refreshPage(){
     window.location.reload();
 } 
 
+var words = [];
+
 function addWord() {
     var node = document.createElement("li");
     var text = document.getElementById("inputField").value.toLowerCase(); 
     var textnode=document.createTextNode(text);
     node.appendChild(textnode);
     document.getElementById("listItems").appendChild(node);
+    words.push(text);
 }
 
 function searchWord() {
     $(".message-successful-inquiry").css("visibility", "hidden");
     $(".message-insuccessful-inquiry").css("visibility", "hidden");
-    var text = document.getElementById("searchWordBar").value.toLowerCase(); 
-    $("#searchedWord").text(text);
-    var ul = document.getElementById("listItems");
-    var words = ul.getElementsByTagName("li");
-    for (var i = 0; i < words.length; ++i) {
-        if (words[i].innerHTML === text) {
-            return $(".message-successful-inquiry").css("visibility", "visible");
-        }
+    var query = document.getElementById("searchWordBar").value.toLowerCase(); 
+    $("#searchedWord").text(query);
+    const found = words.find((word) => word === query);
+    if (found) {
+        return $(".message-successful-inquiry").css("visibility", "visible");
     }
     return $(".message-insuccessful-inquiry").css("visibility", "visible");
 }
